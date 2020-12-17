3cae6ec3198776915dc1191d5fd7f1b6f5f97b19
phedrakeson <gabrield.developer@gmail.com>
2020-12-17 13:46:26 -0300

    initial commit, functions done

---
+++ buscaCep.js
@@ -0,0 +1,26 @@
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('buscarCep');
+
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep)
+}
+
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(r => r.json())
+    .then(dadosCep => {
+      document.querySelector('[data-cep]').innerText = dadosCep.cep;
+      document.querySelector('[data-log').innerText = dadosCep.logradouro;
+      document.querySelector('[data-bairro').innerText = dadosCep.bairro;
+      document.querySelector('[data-local').innerText = dadosCep.localidade;
+      document.querySelector('[data-uf').innerText = dadosCep.uf;
+      document.querySelector('[data-ddd').innerText = dadosCep.ddd;
+
+    })
+}
